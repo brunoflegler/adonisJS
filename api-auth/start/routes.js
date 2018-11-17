3d3cfd6ac8133463b012e5a2b89ee2bca494c84f
Bruno Flegler DalCol <bruno.dalcol@el.com.br>
2018-11-17 16:31:23 -0200

    initial commit

---
+++ api-auth/start/routes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use("Route");
+
+Route.post("/users/register", "AuthController.register");
+
+Route.post("/users/authenticate", "AuthController.authenticate");
+
+Route.get("/app", "AppController.index").middleware(["auth"]);
